466f6583cc8b0e414461e5c7f10f5d93e0aa0f78
Jude Ramanan <juder.ramanan@gmail.com>
2025-05-24 10:40:48 -0400

    Polynomial Functionality

--- EcClairvaux/PolynomialFiniteFieldArithmetic.py
+++ EcClairvaux/PolynomialFiniteFieldArithmetic.py
@@ -1,4 +1,23 @@
 class PolynomialFiniteFieldArithmetic: 
     
     class Poly:
-        pass
+        
+        def __init__(self, coefArr):
+            self.cx = [i for i in coefArr]
+
+        def __str__(self):
+            e = len(self.cx)-1
+            str = ""
+            flag = False
+            for i in self.cx[::-1]:
+                if i!= 0:
+                    if not flag:
+                        str+=(f"{i}x^{e}")
+                        flag = not flag
+                    else:
+                        str+=(f" + {i}x^{e}")
+                e-=1
+            if (str == ""):
+                return "Empty Polynomial"
+            else:
+                return str
---
+++ tests/tests_PolynomialFiniteFieldArithmetic.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from EcClairvaux import PolynomialFiniteFieldArithmetic as pffa
+
+poly = pffa.Poly([0, 0, -15])
+print(poly)
